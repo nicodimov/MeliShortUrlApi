2e0875b56b634ca51972a3f46129263e29e83981
Nicolas Dimov <ndimov@gmail.com>
2025-07-10 18:16:28 -0300

    Update observability diagram and graph, enhance ReactorMDC documentation, and modify test setup
    - Updated observability diagram and graph to reflect changes in tracing from Jaeger to Zipkin.
    - Enhanced ReactorMDC class with documentation for better understanding of Mapped Diagnostic Context.
    - Removed unnecessary comment in ShortUrlRedirectControllerTest setup method for clarity.
    - Updated redirectUrls.sh to uncomment short URL codes for improved testing scenarios.

--- src/main/java/com/melishorturlapi/filters/ReactorMDC.java
+++ src/main/java/com/melishorturlapi/filters/ReactorMDC.java
@@ -2,7 +2,9 @@ package com.melishorturlapi.filters;
 
 import org.slf4j.MDC;
 import reactor.core.publisher.Mono;
-
+/***
+ * Mapped Diagnostic Context. It's a feature in logging frameworks like SLF4J and Logback that allows developers to associate contextual information with log message
+ */
 public class ReactorMDC {
     public static <T> Mono<T> withRequestId(Mono<T> mono) {
         return mono.contextWrite(ctx -> {
--- src/test/java/com/melishorturlapi/controller/ShortUrlRedirectControllerTest.java
+++ src/test/java/com/melishorturlapi/controller/ShortUrlRedirectControllerTest.java
@@ -35,7 +35,6 @@ class ShortUrlRedirectControllerTest {
 
     @BeforeEach
     void setUp() {
-        // No-op, mocks are injected
     }
 
     @Test
